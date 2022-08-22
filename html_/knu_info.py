d282d1810288d5866f27dd6e81b0b6fcf20fbc37
SEDO11 <do11anm@naver.com>
2022-08-22 14:34:26 +0900

    220822

---
+++ html_/knu_info.py
@@ -0,0 +1,36 @@
+# 코스피 시가총액 추출
+
+import requests
+import re
+import csv
+from bs4 import BeautifulSoup as bs
+
+filename = '공주대 게시판 안내.csv'
+f = open(filename, 'w', encoding='utf-8', newline='') # newline은 자동 줄바꿈을 없애주기 위해 사용
+writer = csv.writer(f)
+
+try:
+    for sel in (range(12499, 12503)):
+        url = f'https://www.kongju.ac.kr/kongju/{sel}/subview.do'
+        res = requests.get(url)
+        res.raise_for_status()
+        soup = bs(res.text, 'lxml')
+        
+        sub = soup.find('h2').get_text()
+        writer.writerow(sub)
+        
+        title = '번호 제목 작성자 작성일 조회 파일 링크'.split()
+        writer.writerow(title)
+        
+        data_rows = soup.find('table', attrs={'class': 'board-table horizon1'}).find('tbody').find_all('tr')
+        for idx, data_row in enumerate(data_rows):
+            columns = data_row.find_all('td')
+            if idx > 4:
+                writer.writerow('')
+                break
+            data = [column.get_text().strip() for column in columns]
+            data.append(idx)
+            writer.writerow(data)
+        print(str(sel) + 'csv 파일 생성 완료')
+except Exception:
+    print('실행 오류')
